5ec497fb709ef657d0c0cbec00c1080c4030ea48
Jennie Hellqvist <je.hellqvist@gmail.com>
2016-03-03 12:45:26 +0100

    login prorotyp ui

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -3,56 +3,36 @@
 
 angular.module('LoginCtrl', []).controller('LoginController', function($scope, $location, $http, Login) {
 	
-	// Variable that controls the design of the fields
-	var color = "white";
-	
-	// Updates the color in GUI (Style/Css)
-	$scope.style = function(red) {
-          return { "background-color": color };
-      };
+	// Variable that resets error message
+    $scope.errorMsg = "Enter your credentials";
+    $scope.class ="login--hide"
 
 	// Function that triggers the API login call and manages GUI changes
-	var loginFunc = function() {
+	$scope.loginFunc = function() {
+        
+        $scope.errorMsg = "Enter your credentials";
+        $scope.class = "login--hide"; //reset class
 					
 		Login.get($scope.email, $scope.password).success(function(response) {
 			
 			// login successful
 			if(response == true){
-				color = "green";
-				
+
 				// Redirects to profile
 				$location.path('/profile');
 			}
 			
 			// Login failed
 			else {
-				color = "red";
+                $scope.errorMsg = "Wrong email or password, please try again"; //update error message
+                $scope.class = "login--show"; //show error message
 				console.log("Tried to log in, did not work");
 			}
     	});	
 	}
-
-	// Timer
-	var timer;
-	
-	// Gui Function that fires on key event from GUI. 
-	$scope.checkIfValid = function() {
-		
-		/* Resets the timer
-		clearTimeout(timer);
-		
-		// Sets the timer to 0.4 sec and then checks against DB
-		timer = setTimeout(runCheckIfVaild, 400);*/
-        if(($scope.email != undefined) && ($scope.email !== "")){
-			if(($scope.password != undefined) && ($scope.password !== "")){
-	        	loginFunc();
-			}
-		}
-	}
-	
-	/* Function to run username and password against DB
-	var runCheckIfVaild = function() {
-
-			
-    };*/
+    
+    $scope.forgotPassword = function() {
+        console.log("forgot password func, placeholder")
+    }
+    
 });
