562262fdf0e1c592c61db84851baa87d1839c0c7
Elle <michelle.pantorp@bredband.net>
2016-03-09 16:23:17 +0100

    Updated profile

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -7,9 +7,13 @@ angular.module('ProfileCtrl', []).controller('ProfileController', function($scop
 		if(data != false) {
 			$scope.first_name = data.first_name;
             $scope.last_name = data.last_name;
+            $scope.email = data.email;
+            $scope.phone_number = data.phone_number;
             $scope.url = data.public_url;
+            $scope.description = data.description;
 			$scope.user = data;
 			$scope.role = data.role;
+            $scope.personality = data.personality;
 
 		}else {
 			$scope.first_name = "No profile found";
