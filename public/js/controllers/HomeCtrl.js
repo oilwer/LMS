a3fc60b9a5675235171712d1ba132a12160a92e5
Jennie Hellqvist <je.hellqvist@gmail.com>
2016-03-09 13:25:55 +0100

    dashboard html/css

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 // public/js/controllers/ProfileCtrl.js
 angular.module('HomeCtrl', []).controller('HomeController', function($scope) {
 
-	$scope.test = "hej";
+	$scope.test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco [...]";
 });
