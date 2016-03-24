c8effa0494e780b20bfc44baf0bbd8459951b516
JimmieC <james@midokura.com>
2016-03-24 15:08:18 +0100

    Profile refactor commit#1
    This commit includes public profile refactrored completely
    and the private profile is almost done. All that is needed
    is the login api that when called with get will return the
    logged in user. From that we can get the id

---
+++ www/widgets/profile/privateprofile.js
@@ -0,0 +1,155 @@
+app.directive('profilePrivateprofile', [
+  "settings",
+  "User",
+  "$http",
+  function(
+    settings,
+    User,
+    $http ) {
+
+    return {
+      templateUrl: settings.widgets + 'profile/privateprofile.html',
+      link: function($scope, element, attrs) {
+      		 
+      		//Controlls that switches input
+		    $scope.descriptionEnabled = true;
+		    $scope.contactEnabled = true;
+		    $scope.linksEnabled = true;
+		    $scope.class = "fa fa-pencil";
+		    $scope.contact_class = "fa fa-pencil";
+		    $scope.links_class = "fa fa-pencil";
+		    $scope.isStudent = false;
+		    $scope.homepage = "http://www.myhomepage.com";
+		    var obj = null;
+
+
+		    var getUser = function () {
+
+ 				var fetchedUser = $http.get('/api/login');
+ 				console.log(fetchedUser);
+		    
+		        console.log("Dashboard config");
+		        console.log(fetchedUser.dashboard_config);
+	        
+	        	// recover Users ID from http get on login
+				var userId = fetchedUser.userid;
+				
+				//console.log("SessionID: "+sess.userid);
+				
+				if( (userId !== "") && (userId !== null))	{
+					return fetchedUser;
+				} else {
+					return false;
+				}	
+		    }
+
+		    // Get profile data from DB
+		    var initializeProfile = function (data) {
+		        if (data != false) {
+		            $scope.first_name = data.first_name;
+		            $scope.last_name = data.last_name;
+		            $scope.email = data.email;
+		            $scope.phone_number = data.phone_number;
+		            $scope.url = data.public_url;
+		            $scope.description = data.description;
+		            $scope.user = data;
+		            obj = data;
+		            $scope.role = data.role;
+		            
+		            if(data.role == "Student")
+		                {
+		                    $scope.isStudent = true;
+		                }
+		            $scope.personality = data.personality;
+
+		        } else {
+		            $scope.first_name = "No profile found";
+		        }
+		    };
+			
+			if(getUser()){
+				initializeProfile(getUser());
+			}
+			
+
+
+		    $scope.editDescription = function () {
+		        if ($scope.class == "fa fa-pencil") {
+		            $scope.class = "fa fa-check"
+		            $scope.descriptionEnabled = false;
+		        } else {
+		            obj.description = $scope.description;
+		            $scope.updateProfile(obj);
+		            $scope.descriptionEnabled = true;
+		            $scope.class = "fa fa-pencil"
+		        }
+		    };
+		    
+		    $scope.editContact = function () {
+		        if ($scope.contact_class == "fa fa-pencil") {
+		            $scope.contact_class = "fa fa-check"
+		            $scope.contactEnabled = false;
+		        } else {
+		            obj.phone_number = $scope.phone_number;
+		            obj.public_url = $scope.url;
+		            $scope.updateProfile(obj);
+		            $scope.contactEnabled = true;
+		            $scope.contact_class = "fa fa-pencil"
+		        }
+		    };
+		    
+		    $scope.editLinks = function () {
+		        if ($scope.links_class == "fa fa-pencil") {
+		            $scope.links_class = "fa fa-check"
+		            $scope.linksEnabled = false;
+		        } else {
+		            $scope.updateProfile(obj);
+		            $scope.linksEnabled = true;
+		            $scope.links_class = "fa fa-pencil"
+		        }
+		    };
+
+
+		    //Gui function update profile
+		    $scope.updateProfile = function (obj) {
+		        //Asks UserService to update User
+		        var user = obj;
+
+		        User.update({
+                    //searchObject
+                    _id: user._id
+                },{
+                  //properties
+                      profilePic: user.profilePic,
+                      email: user.email,
+                      first_name: user.first_name,
+                      last_name: user.last_name,
+                      description: user.description,
+                      personality: user.personality,
+                      phone_number: user.phone_number,
+                      password: user.password,
+                      public_url: user.public_url,
+                        courses: {
+                          course_name: user.course_name
+                      },
+                      dashboard_config:[{
+                          plug:{
+                              id: user.id,
+                              name: user.name,
+                              path: user.path,
+                              isActive: user.Boolean,
+                              x: user.x,
+                              y: user.y
+                          }
+                      }],
+                      role: user.role //student/admin/teacher
+                  });
+
+		        if(user != null){	        	
+		            $scope.user = user;
+		        }
+		    };
+      }
+    };
+  }
+]);
