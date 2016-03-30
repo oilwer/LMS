b9216762e5d26027dd5ec736eebcd7c80b8027fd
ahlberg <oliver@charlesweb.se>
2016-03-30 11:20:10 +0200

    Added working permissions

--- permissions/toAsk.js
+++ permissions/toAsk.js
@@ -1,4 +1,47 @@
 module.exports = function(modelName, method, query, rbody) {
-  console.log("ask", modelName, method, query, rbody);
-  return true;
+	
+	// If somebody not logged in trying to do API calls - block them
+	if((rbody.session.user == undefined))
+	{
+		console.log("Blocked API access from unauthorized user");
+		return false;
+	}
+	
+	// If logged in
+	if(rbody.session.user != undefined)
+	{
+		// If admin - Allow anything
+		if(rbody.session.user.role == "admin")
+		{
+			return true;
+		}
+		
+		// Not admin
+		else
+		{
+				
+			if(method == "GET")
+			{
+				if(query._id == rbody.session.user._id)
+				{
+					console.log("ASK full get on himself", modelName, method, query);
+					return true;
+				}	
+				else
+				{
+					console.log("Full get on somebody else");
+					return false;
+				}
+			} 
+		}
+		
+	}
+  
+ /* if((modelName == "User") && (method == "GET") && (rbody.session.user == undefined))
+  {
+	  console.log("We are not logged in");
+  }
+  */
+   // console.log("ask:", modelName, method);
+  // return true;
 };
