f5b8434a4517848deca1e1326afe176254fc91ea
ahlberg <oliver@charlesweb.se>
2016-03-31 14:31:55 +0200

    Working on connecting students

--- www/widgets/connect/studentsaddremove.js
+++ www/widgets/connect/studentsaddremove.js
@@ -1,98 +1,86 @@
 app.directive('connectStudentsaddremove', [
   "settings",
   "User", 
+  "Course",
   function(
     settings,
-    User
+    User,
+    Course
   ) {
 	  
     return {
       templateUrl: settings.widgets + 'connect/studentsaddremove.html',
       link: function(scope, element, attrs) {
           
-          
-         
-          
 	  		
-	      scope.students = [{
-              name: "Student 1",
-             
-          }, {
-              name: "Student 2",
-              
-          }, {
-              name: "Student 3",
-              
-          }, {
-              name: "Student 4",
-         
-          }, {
-              name: "Student 5",
-                   
-          }, {
-              name: "Student 6",
-                       
-          }, {
-              name: "Student 7",
-                       
-          }, {
-              name: "Student 8",
-          }, {
-              name: "Student 2",
-              
-          }, {
-              name: "Student 3",
-              
-          }, {
-              name: "Student 4",
-         
-          }, {
-              name: "Student 5",
-                   
-          }, {
-              name: "Student 6",
-                       
-          }, {
-              name: "Student 7",
-                       
-          }, {
-              name: "Student 8",
-          }, {
-              name: "Student 2",
-              
-          }, {
-              name: "Student 3",
-              
-          }, {
-              name: "Student 4",
-         
-          }, {
-              name: "Student 5",
-                   
-          }, {
-              name: "Student 6",
-                       
-          }, {
-              name: "Student 7",
-                       
-          }, {
-              name: "Student 8",
-              
-          }];
+	      
+	      
+	    User.get({},function(users){
+		  	scope.students = users;
+		});
+		
+			
+		Course.get({name: "epiccourse", _populate:"students"},function(course){
+			console.log(course[0].students);
+		  	scope.studentsToBeAdded = course[0].students;
+		});
+	                
+          // scope.studentsToBeAdded = [];
+
           
-          
-          scope.addUsers = function()
-          {
-              alert("Adding users");
-              console.log(scope.students);
-          }
-          
-          
-          
-          
-      }
-  
-    }
+          // Add Item to Checked List and delete from Unchecked List
+    scope.stageMeToCourse = function (index) {
+	    
+	    alert(index);
+	    
+	  //  scope.studentsToBeAdded.push(scope.students[index]);
+	    
+	    Course.get({name: "epiccourse"},function(course){
+	              			            
+						 	
+						 	Course.update({_relate:{items:course[0],students:scope.studentsToBeAdded}},function(res){
+						 		console.log(res);
+						 		console.log("updated");
+						 		
+						 		User.update({_relate:{items:scope.studentsToBeAdded[index],courses:course[0]}},
+						 		function(newres)
+						 		{
+							 		console.log(newres);
+							 	});
+
+						 	});
+		             
+	              });
+
+	    
+        
+        scope.students.splice(index, 1);
+    };
+    
+              // Add Item to Checked List and delete from Unchecked List
+    scope.unstageMeToCourse = function (index) {
+	    
+	    console.log(index);
+	    
+	     scope.students.push(scope.studentsToBeAdded[index]);
+	    	    
+    	Course.update(
+        	{name: "epiccourse"},
+        	{ $pull: { 'students': scope.studentsToBeAdded[index]._id}}, function(res)
+        	{
+	        	console.log(res);
+        	}
+    	);
+	    
+        
+        scope.studentsToBeAdded.splice(index, 1);
+    	};
+    
+		    	
+	    }
+	    
+	    }
+
 }
 ]);
 
