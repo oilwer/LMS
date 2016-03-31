53bdca8d06ae8fae4da71bb1e5ed1750c17ee83e
Jennie Hellqvist <je.hellqvist@gmail.com>
2016-03-31 15:55:52 +0200

    US25 process

--- www/widgets/createcourse/createcourse.js
+++ www/widgets/createcourse/createcourse.js
@@ -11,25 +11,42 @@ app.directive('createCourse', [
     Course,
     SessionService
   ) {
+        
+        
 
     return {
       templateUrl: settings.widgets + 'createcourse/createcourse.html',
       link: function(scope, element, attrs) {
         
+          
+          // CURRENT COURSE VARIABLES
+          //
+          // session_user : creates by system CHECK
+          // id : creates by system
+          // name : view
+          // description : view
+          // start, end : view
+          //
+          
         var session_user;
         SessionService.getSession().success(function(response){
             session_user = response.user;
         });
           
+        
+        //completed steps
+        var stepOne = false; //choose to create or copy course
+        var stepTwo = false; //create course
+        var stepTree = false; //preview course
           
-
+        //All the avaible steps in the create course process
         scope.steps = [
             'Step 1: Create or copy',
             'Step 2: Course details',
             'Step 3: Preview'
           ];
           
-          //start out on step - update to represent current position in process when re-enter
+        //start out on step - update to represent current position in process when re-enter
         scope.selection = scope.steps[0];
 
         scope.getCurrentStepIndex = function(){
@@ -42,8 +59,7 @@ app.directive('createCourse', [
           scope.selection = scope.steps[index];
         };
           
-
-        
+          
         scope.hasNextStep = function(){
             var stepIndex = scope.getCurrentStepIndex();
             var nextStep = stepIndex + 1;
@@ -68,23 +84,75 @@ app.directive('createCourse', [
             };
         };
           
+        //next step
          scope.incrementStep = function() {
             if ( scope.hasNextStep() )
             {
               var stepIndex = scope.getCurrentStepIndex();
               var nextStep = stepIndex + 1;
               scope.selection = scope.steps[nextStep];
+                //if-else depending on step taken
+                //Step 1.
+                //create new course locally
+                //Step 2.
+                //create new course with the information that is required for the minimum creating
+                //Step 3:
+                //show details
             }
           };
-
+          
+          //previous step
           scope.decrementStep = function() {
             if ( scope.hasPreviousStep() )
             {
               var stepIndex = scope.getCurrentStepIndex();
               var previousStep = stepIndex - 1;
               scope.selection = scope.steps[previousStep];
+                //check if any changes are made and ask for update
             }
           };
+          
+          
+        
+        var createCourse = function(){
+          var course = scope.course;
+            Course.create({
+              status: false,
+              code: course.code,
+              creator: session_user._id
+            });
+            
+            scope.courselist.push(scope.course); 
+                 refresh();
+            scope.course = "";
+            course = null;
+        };
+
+        var updateCourse = function(){
+          scope.btnAddOrUpdateTextCourse = 'Add course';
+            var course = scope.course;
+            console.log(course);
+             Course.update({
+                _id: course._id
+              },{
+                status: true,
+                code: course.code
+            });
+            // Refresh GUI
+            refresh();
+            scope.course = "";
+            isEditingCourse = false;
+        }
+
+        //Gui function add course
+        scope.addOrUpdateCourse = function(){
+          if(!isEditingCourse){
+            createCourse();
+          }
+          else{
+            updateCourse();
+          }
+        }
           
       }//end link
     };
