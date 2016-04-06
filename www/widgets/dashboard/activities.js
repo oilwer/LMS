a2b06a688064c2fe3c32f4414d85843afa787a1a
JimmieC <james@midokura.com>
2016-04-06 13:47:50 +0200

    Activity Course only Teacher Admin
    The activity button to add a new course is limited to a teacher
    or admin, this uses ng-if to remove it if the user is a student

--- www/widgets/dashboard/activities.js
+++ www/widgets/dashboard/activities.js
@@ -2,17 +2,22 @@ app.directive('dashboardActivities', [
     "settings",
     "$location",
     "$window",
+    "SessionService",
   function(
     settings,
     $location,
-    $window
+    $window,
+    SessionService
   ) {
 
     return {
       templateUrl: settings.widgets + 'dashboard/activities.html',
       link: function(scope, element, attrs) {
-        
-          
+        SessionService.getSession().success(function(response){
+          if(response.user.role == "admin" || response.user.role == "teacher"){
+            scope.buttonDisplay = true;
+          }
+        });
       }
     };
   }
