2873f7a760c283e35721f0778b3c085b5ed4ddfe
Elle <michelle.pantorp@bredband.net>
2016-04-07 21:38:03 +0200

    breadcrumbs_fix

--- www/widgets/breadcrumbs/breadcrumbs.js
+++ www/widgets/breadcrumbs/breadcrumbs.js
@@ -11,34 +11,23 @@ app.directive('breadcrumbsBreadcrumbs', [
       link: function(scope, element, attrs) {
     
       //Breadcrumbs
-      scope.locations = $location.path().split('/').splice(1);
-        scope.locationspath = [];
-        for (i = 0; i < scope.locations.length; i++){
+      var locations = $location.path().split('/').splice(1);
+      var locationspath = [];
+        for (i = 0; i < locations.length; i++){
             if (i > 0){
-                scope.locationspath.push(scope.locations[i-1] + "/" + scope.locations[i]);
+                locationspath.push(locations[i-1] + "/" + locations[i]);
 
             }
             else{
-                scope.locationspath.push(scope.locations[i]);
+                locationspath.push(locations[i]);
             }                          
         }
-         /* console.log("locations " + scope.locations);
-          console.log("locationspath " + scope.locationspath);
-          scope.locObj = {};
-          scope.locObj.data = [];
-          scope.locObj.value = [];
-          for(i = 0; i < scope.locations.length; i++){
-              scope.locObj.data[i] = scope.locations[i];
-              scope.locObj.value[i] = scope.locationspath[i];
-          }
-
           
-      console.log("LocObj " + scope.locObj.data + scope.locObj.value)*/
-      /*scope.firstLoc = loc[1];
-      scope.secondLoc = loc[2];
-      scope.firstLoc_view = loc[0].charAt(0).toUpperCase() + loc[0].slice(1);
-      scope.secondLoc_view = loc[1].charAt(0).toUpperCase() + loc[1].slice(1);*/
-
+       scope.locObj = {};
+          
+       for(i = 0; i < locations.length; i++){
+            scope.locObj[locations[i]] = locationspath[i];
+       }
        
     }
   }
