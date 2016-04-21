27304b419919c43dde7a0b9194f9cbe8ed7e75a4
ahlberg <oliver@charlesweb.se>
2016-04-21 15:47:11 +0200

    Added slack as a modelanything plugin

--- www/widgets/modelanything/modelanythingstart.js
+++ www/widgets/modelanything/modelanythingstart.js
@@ -7,12 +7,12 @@ app.directive('modelanythingModelanythingstart', [
     User,
     SessionService
   ) {
-  
+
     return {
       templateUrl: settings.widgets + 'modelanything/modelanythingstart.html',
       link: function(scope, element, attrs) {
-	      
-	  		
+
+
         SessionService.getSession().success(function(response){
           //gets the users id from current sessions
           //console.log(response.user._id);
@@ -27,25 +27,24 @@ app.directive('modelanythingModelanythingstart', [
 
   			  scope.html = "";
   			  // console.log(user);
-  			  
+
          // console.log(user.plugs.length);
 
   			  for(var i = 0; i < user.plugs.length; i++) {
   				  if(user.plugs[i].isActive == true){
   					  // Adds html to all plugins
   					  var plug = user.plugs[i].name;
-  					  var id = user.plugs[i]._id;
+  					  var id = user.plugs[i].id;
   					  console.log(plug);
   					  scope.html += "<div class='plugin' id='" + plug + id + "'> <" + "modelanything-plugins-" + plug + "/> </div>";
   			  	  }
   			  }
   		      //scope.html = '<div class="col-xs-12" form-base>';
 	      }
-	      
+
 	      // User.onQueueDone (loadPlugs);
   	      // console.log(kk);
       }
     };
   }
 ]);
-
