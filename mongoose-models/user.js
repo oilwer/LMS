ba7c825657cbe2173829a8aa98223bab1fe257bf
ahlberg <oliver@charlesweb.se>
2016-04-27 09:39:37 +0200

    Bug fix in user

--- mongoose-models/user.js
+++ mongoose-models/user.js
@@ -46,12 +46,16 @@ module.exports = function(mongoose){
   });
 
   UserSchema.pre('update', function(next){
-    console.log(this._update.$set.password);
-    if (!this._update.$set.password) {
-      console.log ("Not hashing ", this._update.$set.password);
-    }
-    else {
-      this._update.$set.password = sha1(this._update.$set.password);
+
+    if (this._update.$set != null){
+    console.log("Set undefined");
+
+      if (!this._update.$set.password) {
+        console.log ("Not hashing ", this._update.$set.password);
+      }
+      else {
+        this._update.$set.password = sha1(this._update.$set.password);
+      }
     }
 
     next();
