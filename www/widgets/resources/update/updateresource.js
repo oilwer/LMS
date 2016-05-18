8f245f4c4ad52082fac992fcef6047659c5feaa7
ahlberg <oliver@charlesweb.se>
2016-05-18 11:02:05 +0200

    Fixed resource desc update

--- www/widgets/resources/update/updateresource.js
+++ www/widgets/resources/update/updateresource.js
@@ -15,32 +15,35 @@ app.directive('resourcesUpdateUpdateresource', [
      Course,
     SessionService
   ) {
-        
+
     return {
       templateUrl: settings.widgets + 'resources/update/updateresource.html',
       link: function(scope, element, attrs) {
-          
-        
+
+
         var setupUpdate = function() {
             console.log("the resource", scope.theResource);
+
             scope.newResource = {
             name: scope.theResource.title,
             file: scope.theResource.filename,
             content: scope.theResource.content
         };
-          
+
             //update view
             if(scope.theResource.content !== undefined) {
-                $("#resourceContent").attr("value", scope.theResource.content);
+
+              var textEditor = document.querySelector("trix-editor[input='resourceContent']");
+              textEditor.editor.insertHTML(scope.theResource.content);
             }
             console.log(scope);
-            
+
         }
-        
+
         scope.$root.$on('setupUpdateScope', function() {
             setupUpdate();
         });
-          
+
       scope.updateResourceDetails = function() {
           console.log("kör update");
         if (scope.file) {
@@ -51,17 +54,17 @@ app.directive('resourcesUpdateUpdateresource', [
         else {
             var strippedFileName = undefined;
         }
-          
+
         scope.newResource.file = strippedFileName;
         //create function
         scope.newResource.content = $("#resourceContent").attr("value");
-                          
+
         Resource.update({_id: scope.theResource._id}, {
             title: scope.newResource.name,
             filename: scope.newResource.file,
             content: scope.newResource.content
         }, function(res)
-        {            
+        {
             scope.$root.$broadcast('reloadTheResource');
             //todo: show user the success (GUI)
             //update view
@@ -73,12 +76,14 @@ app.directive('resourcesUpdateUpdateresource', [
             content: scope.newResource.content
         };
             scope.$parent.hideModal();
-    
+
         });
-          
+
       };
-          
+
+
         scope.closeUpdateResource = function() {
+
             console.log("körs");
             if (confirm('Do you want to close without saving?')) {
                 scope.$parent.hideModal();
@@ -92,25 +97,25 @@ app.directive('resourcesUpdateUpdateresource', [
                 textEditor.editor.insertHTML(scope.theResource.content);
             } else {
                 // Do nothing!
-            } 
+            }
         };
-        
+
           scope.deleteResource = function() {
               Resource.remove({_id: scope.theResource._id});
-              
+
               Course.update(
-                  {_id: scope.theResource.course}, 
+                  {_id: scope.theResource.course},
                   {$pull: {"resources": scope.theResource._id}}, function() {
                       scope.$parent.hideModal();
                       //get path without last segment
                       var theLocation = $location.path().split("/")
                       theLocation.pop();
                       theLocation = theLocation.join("/");
-                      $location.path(theLocation); 
+                      $location.path(theLocation);
               });
-              
+
           };
-          
+
       }//end link
     };
   }
